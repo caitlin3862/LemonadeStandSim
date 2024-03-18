81c19209d1100b69005c9354318b371b2f488c7f
BT_2N2_30 <sample@bths.edu>
2024-03-18 14:04:14 -0400

    Added classes and instance variables

---
+++ src/Customer.java
@@ -0,0 +1,6 @@
+public class Customer {
+    private String order;
+    private String type;
+    private int quantity;
+    private boolean isPinkLemonade;
+}
---
+++ src/Inventory.java
@@ -0,0 +1,8 @@
+public class Inventory {
+    private int lemons;
+    private int cups;
+    private int sugarCubes;
+    private int iceCubes;
+    private int pinkDye;
+
+}
